afb57d9787f83b852e7f7367b8d72ec75ba1cb2a
Sac1802 <saidacostacepeda03@gmail.com>
2025-10-19 23:48:10 -0500

    feat: Implemented logic move enemy

--- src/combat_screen/CombatScreen.py
+++ src/combat_screen/CombatScreen.py
@@ -1,36 +1,72 @@
-import os
-
 import pygame
 import socket
 import json
 
+import encryptAES.manageAES
+import udp_service.udp_service
+
 class CombatScreen:
+
     def __init__(self, game):
         self.game = game
         self.font = pygame.font.Font(None, 32)
 
-        self.background = pygame.image.load("/home/sac/Downloads/clientCN2/pygame-stick-game/src/sprites/enviroment/nature_5/orig.png").convert()
+        self.background = pygame.image.load(
+            "/home/sac/Downloads/clientCN2/pygame-stick-game/src/sprites/enviroment/nature_5/orig.png"
+        ).convert()
 
-        self.player_image = pygame.image.load("/home/sac/Downloads/clientCN2/pygame-stick-game/src/sprites/sprite_player/sprite_07.png").convert()
-        self.player_pos = pygame.Vector2(300, 300)
+        self.player_image = pygame.image.load(
+            "/home/sac/Downloads/clientCN2/pygame-stick-game/src/sprites/sprite_player/sprite_07.png"
+        ).convert_alpha()
+
+        self.player1_pos = pygame.Vector2(200, 300)
+        self.player2_pos = pygame.Vector2(600, 300)
+
+        self.player2_image = pygame.transform.flip(self.player_image, True, False)
 
     def handle_event(self, event):
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 self.game.set_screen("login")
+
             elif event.key == pygame.K_UP:
-                self.player_pos.y -= 10
+                self.player1_pos.y -= 10
+            elif event.key == pygame.K_DOWN:
+                self.player1_pos.y += 10
             elif event.key == pygame.K_LEFT:
-                self.player_pos.x -= 10
+                self.player1_pos.x -= 10
             elif event.key == pygame.K_RIGHT:
-                self.player_pos.x += 10
+                self.player1_pos.x += 10
+
+    def handled_player2(self):
+        positions = udp_service.udp_service.return_value()
+        self.player2_pos.x = positions["positionX"]
+        self.player2_pos.y = positions["positionY"]
 
     def update(self):
         pass
 
     def draw(self, screen):
-        screen.fill((0, 0, 0))
         screen.blit(self.background, (0, 0))
-        screen.blit(self.player_image, self.player_pos)
-        screen.blit(self.player_image, self.player_pos)
-        screen.blit(self.font.render("Juego de combate (ESC para salir)", True, (255,255,255)), (100, 100))
+
+        screen.blit(self.player_image, self.player1_pos)
+
+        screen.blit(self.player2_image, self.player2_pos)
+
+        # Texto
+        text = self.font.render("Juego de combate (ESC para salir)", True, (255,255,255))
+        screen.blit(text, (100, 100))
+
+    def send_position(self):
+        data_transfer = {
+            "IdPlayer": player_id,
+            "eventType": "PLAYER_MOVE",
+            "payload": {
+                "x": self.player1_pos.x,
+                "y": self.player1_pos.y,
+                "direction": direction
+            }
+        }
+        data_transfer.update(data_transfer)
+
+
--- src/udp_service/udp_service.py
+++ src/udp_service/udp_service.py
@@ -2,22 +2,35 @@ import socket
 import threading
 import time
 
+import encryptAES.manageAES
+import generate_rsa.generate_rsa
+
 SERVER_ADDRESS = "127.0.0.1"
 SERVER_PORT = 5005
-
+key = ""
+iv = ""
+message = ""
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client_socket.setblocking(False)
 
 def receive_message():
+    global key, iv, message
     while True:
         try:
             data, addr = client_socket.recvfrom(1024)
-            print(f"Message send to {addr}: {data.decode()}")
+            message = data
+            if b":" in data:
+                parts = data.split(b":", 1)
+                key = generate_rsa.generate_rsa.decrypt_rsa(parts[0])
+                iv = generate_rsa.generate_rsa.decrypt_rsa(parts[1])
+            return_value()
+            print(f"Message received from {addr}: {data.decode()}")
         except BlockingIOError:
             time.sleep(0.01)
         except Exception as e:
-            print(f"Error to receipt: {e}")
+            print(f"Error receiving: {e}")
             break
+
 
 threading.Thread(target=receive_message, daemon=True).start()
 
@@ -27,6 +40,9 @@ def send_message(message: str):
         print(f"Send: {message}")
     except Exception as e:
         print(f"Error to send: {e}")
+
+def return_value():
+    return encryptAES.manageAES.decrypt(message, key, iv)
 
 def close_socket():
     client_socket.close()
